16e9c114b3df4ec3d08f0c80d492842b3118675e
Wong Hong You <wonghongyou@gmail.com>
2023-07-04 22:00:58 +0800

    ex01 done

---
+++ ex01/main.cpp
@@ -0,0 +1,37 @@
+#include "iter.hpp"
+#include <iostream>
+#include <cctype>
+
+void ft_tolower( char &arr ) {
+	arr = std::tolower(static_cast<unsigned char>(arr));
+}
+
+void ft_toupper( char &arr ) {
+	arr = std::toupper(static_cast<unsigned char>(arr));
+}
+
+int main( void ) {
+	char a[] = {'A', 'B', 'C'};
+
+	std::cout << "Original:" <<
+	"\n\ta[0]: " << a[0] <<
+	"\n\ta[1]: " << a[1] <<
+	"\n\ta[2]: " << a[2] <<
+	std::endl << std::endl;
+
+	::iter(a, sizeof(a) / sizeof(char), ft_tolower);
+
+	std::cout << "Changed: lower" <<
+	"\n\ta[0]: " << a[0] <<
+	"\n\ta[1]: " << a[1] <<
+	"\n\ta[2]: " << a[2] <<
+	std::endl << std::endl;
+
+	::iter(a, 2, ft_toupper);
+
+	std::cout << "Changed: upper" <<
+	"\n\ta[0]: " << a[0] <<
+	"\n\ta[1]: " << a[1] <<
+	"\n\ta[2]: " << a[2] <<
+	std::endl << std::endl;
+}
